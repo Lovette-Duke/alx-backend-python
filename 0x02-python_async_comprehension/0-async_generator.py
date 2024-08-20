ea292624da340fc1b141e045f0ff4097417de5dc
Lovette Duke <qutyluv@gmail.com>
2024-08-20 15:08:31 +0100

    async correction

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -11,4 +11,4 @@ async def async_generator() -> Generator[float, None, None]:
     """ loop 10 times, wait 1 second and yields a random number b/w 0 and 10"""
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
